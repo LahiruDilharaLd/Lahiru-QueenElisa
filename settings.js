ba9d58d15851979c47f210c604024ce0d6e621ae
Lahiru Dilhara LD <132160250+LahiruDilharaLd@users.noreply.github.com>
2024-09-12 01:40:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
+  MONGODB_URL: 'mongodb+srv://La5244:La@5244785@cluster0.v2wv3.mongodb.net/', 
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,
